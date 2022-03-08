8af029d3e49246854c5c584346e850dddce9399c
Yunus Emre Koçak <yunusemregithub795@gmail.com>
2022-03-08 04:03:46 +0300

    C# Windows Form Sayı Tahmin Etme Oyunu
    C#

---
+++ SayiTaminEtmeOyunu/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace SayiTaminEtmeOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tahmin_et_buton = new System.Windows.Forms.Button();
+            this.basla = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sonuc_text = new System.Windows.Forms.Label();
+            this.sayac_text = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tahmin_et_buton
+            // 
+            this.tahmin_et_buton.Font = new System.Drawing.Font("Calibri", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tahmin_et_buton.Location = new System.Drawing.Point(77, 258);
+            this.tahmin_et_buton.Name = "tahmin_et_buton";
+            this.tahmin_et_buton.Size = new System.Drawing.Size(100, 36);
+            this.tahmin_et_buton.TabIndex = 0;
+            this.tahmin_et_buton.Text = "Tahmin Et";
+            this.tahmin_et_buton.UseVisualStyleBackColor = true;
+            this.tahmin_et_buton.Click += new System.EventHandler(this.tahmin_et_buton_Click);
+            // 
+            // basla
+            // 
+            this.basla.Font = new System.Drawing.Font("Calibri", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.basla.Location = new System.Drawing.Point(77, 115);
+            this.basla.Name = "basla";
+            this.basla.Size = new System.Drawing.Size(107, 36);
+            this.basla.TabIndex = 1;
+            this.basla.Text = "Oyunu Başlat";
+            this.basla.UseVisualStyleBackColor = true;
+            this.basla.Click += new System.EventHandler(this.basla_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(199, 190);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(72, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sayı Giriniz:";
+            // 
+            // sonuc_text
+            // 
+            this.sonuc_text.AutoSize = true;
+            this.sonuc_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sonuc_text.Location = new System.Drawing.Point(450, 157);
+            this.sonuc_text.Name = "sonuc_text";
+            this.sonuc_text.Size = new System.Drawing.Size(0, 24);
+            this.sonuc_text.TabIndex = 4;
+            // 
+            // sayac_text
+            // 
+            this.sayac_text.AutoSize = true;
+            this.sayac_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayac_text.Location = new System.Drawing.Point(479, 109);
+            this.sayac_text.Name = "sayac_text";
+            this.sayac_text.Size = new System.Drawing.Size(0, 24);
+            this.sayac_text.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(374, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kalan Hak:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(374, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sonuç:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 24F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label4.Location = new System.Drawing.Point(225, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(355, 36);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sayı Tahmin Etme Oyunu";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sayac_text);
+            this.Controls.Add(this.sonuc_text);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.basla);
+            this.Controls.Add(this.tahmin_et_buton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tahmin_et_buton;
+        private System.Windows.Forms.Button basla;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label sonuc_text;
+        private System.Windows.Forms.Label sayac_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SayiTaminEtmeOyunu/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTaminEtmeOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int tutulanSayi, sayac;
+
+        private void tahmin_et_buton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                sonuc_text.Text = "Sayı giriniz.";
+            }
+            else
+            {
+                int girilenSayi = int.Parse(textBox1.Text);
+
+                if (girilenSayi < tutulanSayi)
+                {
+                    sonuc_text.Text = "Yukarı";
+                    sayac--;
+                }
+                else if (girilenSayi > tutulanSayi)
+                {
+                    sonuc_text.Text = "Aşağı";
+                    sayac--;
+                }
+                else
+                {
+                    sonuc_text.Text = "TEBRİKLER. KAZANDINIZ!!!";
+                    tahmin_et_buton.Enabled = false;
+                }
+            }
+            sayac_text.Text = sayac.ToString();
+
+            if (sayac == 0)
+            {
+                tahmin_et_buton.Enabled = false;
+                sonuc_text.Text = "Maalesef bilemedin şansını tekrar dene! Tutulan Sayı:" + tutulanSayi;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void basla_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            tutulanSayi = rastgele.Next(1, 100);
+
+            sayac = 5;
+            sayac_text.Text = sayac.ToString();
+            sonuc_text.Text = "";
+            textBox1.Text = "";
+
+           tahmin_et_buton.Enabled = true;
+        }
+    }
+}
